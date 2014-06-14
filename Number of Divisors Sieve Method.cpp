b3c7d58f71795b69495b9b7d3e5aa6f42bb5d1ad
algorithmsdb <algorithmsdb@users.noreply.github.com>
2014-06-14 18:34:48 +0530

    Create Number of Divisors Sieve Method.cpp

---
+++ Number of Divisors Sieve Method.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 10000000   // 10^7
+int numdiv[MAX+10]={0};
+
+int main()
+{
+
+for(int i=1;i<=MAX;i++)
+    {
+    for(int j=i;j<=MAX;j+=i)
+        {
+        numdiv[j]++;
+        }
+    }
+
+for(int i=1;i<=20;i++) cout<<numdiv[i]<<"\n";
+
+}
